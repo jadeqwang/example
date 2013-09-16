f8c06f1b3d304f26c8251ad4bf0ee794ede8456c
Jade <jade@meteor.com>
2013-09-16 12:04:21 -0700

    chapter 4

--- client/views/posts/postbody.js
+++ client/views/posts/postbody.js
@@ -1,29 +1,23 @@
-var postsData = [
-  {
-    title: 'Introducing Telescope',
-    author: 'Sacha Greif',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  }, 
-  {
-    title: 'Meteor',
-    author: 'Tom Coleman',
-    url: 'http://meteor.com'
-  }, 
-  {
-    title: 'The Meteor Book',
-    author: 'Tom Coleman',
-    url: 'http://themeteorbook.com'
-  }
-];
+// var postsData = [
+//   {
+//     title: 'Introducing Telescope',
+//     author: 'Sacha Greif',
+//     url: 'http://sachagreif.com/introducing-telescope/'
+//   }, 
+//   {
+//     title: 'Meteor',
+//     author: 'Tom Coleman',
+//     url: 'http://meteor.com'
+//   }, 
+//   {
+//     title: 'The Meteor Book',
+//     author: 'Tom Coleman',
+//     url: 'http://themeteorbook.com'
+//   }
+// ];
 
 Template.postbody.helpers({
-  posts: postsData
-});
-
-Template.postItem.helpers({
-  domain: function() {
-    var a = document.createElement('a');
-    a.href = this.url;
-    return a.hostname;
+  posts: function(){
+    return Posts.find();
   }
 });
